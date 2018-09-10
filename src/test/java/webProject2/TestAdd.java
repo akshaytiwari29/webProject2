32c69e728d1cd4960abe6c73c51cf5a452465fef
akshaytiwari29 <41627550+akshaytiwari29@users.noreply.github.com>
2018-09-10 11:18:16 +0530

    Update TestAdd.java

--- src/test/java/webProject2/TestAdd.java
+++ src/test/java/webProject2/TestAdd.java
@@ -16,25 +16,7 @@ public class TestAdd {
 		assertEquals(33,a);
 		
 	}
-	                public void test2()
-                {
-                                Sub s=new Sub();
-                                int result2=s.subtract(500, 100);
-                                assertEquals(400,result2);
-                                
-                }
-                public void test3()
-                {
-                                Multiplication m1=new Multiplication();
-                                int result3=m1.mul(24, 2);
-                                assertEquals(48,result3);
-                }
-                public void test4()
-                {
-                                Div d1=new Div();
-                                int result4=d1.division(500, 10);
-                                assertEquals(50,result4);
-                }
+	         
 
 
 
